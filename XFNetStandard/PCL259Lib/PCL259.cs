d4e0d72c95c7033673c46214c119da6ff28c8def
ytabuchi @ Surface <ytabuchi.xlsoft@gmail.com>
2017-11-20 15:38:05 +0900

    Add PCL Xamarin.Forms and PCL library project

---
+++ XFNetStandard/PCL259Lib/PCL259.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PCL259Lib
+{
+    public class PCL259
+    {
+        public static string Hello(string str)
+        {
+            return $"Hello, {str}!";
+        }
+    }
+}
