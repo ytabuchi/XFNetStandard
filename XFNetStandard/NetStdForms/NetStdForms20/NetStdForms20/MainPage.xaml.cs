bf9d3bb55113a4adee206a22ec5fc2283b187cdc
ytabuchi @ Surface <ytabuchi.xlsoft@gmail.com>
2017-11-22 20:30:30 +0900

    Re-generate NET Standard 2.0 Xamarin.Forms project

---
+++ XFNetStandard/NetStdForms/NetStdForms20/NetStdForms20/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NetStdForms20
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void pcl_Clicked(object sender, EventArgs e)
+        {
+            var msg = PCL259Lib.PCL259.GetMessage();
+            await DisplayAlert("PCL", msg, "OK");
+        }
+
+        private async void netstd_Clicked(object sender, EventArgs e)
+        {
+            var msg = NetStd20Lib.NetStd20.GetMessage();
+            await DisplayAlert("NET Standard", msg, "OK");
+        }
+    }
+}
