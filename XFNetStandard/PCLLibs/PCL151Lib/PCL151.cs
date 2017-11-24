5277a9b1eddcea8b116603c4a1ebb9a038eb7810
ytabuchi @ Surface <ytabuchi.xlsoft@gmail.com>
2017-11-24 11:11:12 +0900

    Add PCL 44 and 151 projects

---
+++ XFNetStandard/PCLLibs/PCL151Lib/PCL151.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PCL151Lib
+{
+    public class PCL151
+    {
+        public static string GetMessage() => "Hello from PCL Profile 151!";
+    }
+}
---
+++ XFNetStandard/PCLLibs/PCL44Lib/PCL44.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PCL44Lib
+{
+    public class PCL44
+    {
+        public static string GetMessage() => "Hello from PCL Profile 44!";
+    }
+}
