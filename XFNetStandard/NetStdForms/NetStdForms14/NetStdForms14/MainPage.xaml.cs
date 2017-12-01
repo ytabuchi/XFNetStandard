19257f85af4319ef1cc5c0a85e2c97d01b4629e7
ytabuchi @ Surface <ytabuchi.xlsoft@gmail.com>
2017-12-01 17:10:26 +0900

    add libraries to net standard 1.4 and 2.0 projcts

--- XFNetStandard/NetStdForms/NetStdForms14/NetStdForms14/MainPage.xaml.cs
+++ XFNetStandard/NetStdForms/NetStdForms14/NetStdForms14/MainPage.xaml.cs
@@ -13,5 +13,17 @@ namespace NetStdForms14
         {
             InitializeComponent();
         }
+
+        private async void pcl_Clicked(object sender, EventArgs e)
+        {
+            var msg = PCL259Lib.PCL259.GetMessage();
+            await DisplayAlert("PCL", msg, "OK");
+        }
+
+        private async void netstd_Clicked(object sender, EventArgs e)
+        {
+            var msg = NetStd13Lib.NetStd13.GetMessage();
+            await DisplayAlert("NET Standard", msg, "OK");
+        }
     }
 }
